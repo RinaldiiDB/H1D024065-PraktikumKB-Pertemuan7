c83b9be3af0eaab23d480034ab6a28ba72f83072
RinaldiiDB <rinaldiidb@gmail.com>
2026-05-16 20:28:32 +0700

    Ganti dataset

--- jst-iris.py
+++ jst-iris.py
@@ -9,50 +9,51 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.metrics import confusion_matrix
 
-# Muat dataset iris dari file CSV/URL
-dataset = pd.read_csv('http://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', header=None, sep=',')
+# Muat dataset iris dari file CSV/data
+dataset = pd.read_csv(
+    "iris.data",
+    header=None,
+)
 
 # Menyusun data X (fitur) dan y (label)
-# .iloc[:, :-1] artinya mengambil semua baris dan semua kolom KECUALI kolom terakhir
-X = dataset.iloc[:, :-1].values 
-# .iloc[:, -1] artinya mengambil semua baris hanya pada kolom TERAKHIR
-y = dataset.iloc[:, -1].values
+X = dataset.iloc[:, :-1].values  # 4 kolom pertama sebagai fitur
+y = dataset.iloc[:, -1].values  # Kolom terakhir sebagai label
 
 # Mengonversi label dari string menjadi numerik
 label_encoder = LabelEncoder()
-y = label_encoder.fit_transform(y) # Mengubah label jadi 0, 1, 2
+y = label_encoder.fit_transform(y)  # Mengubah label jadi 0, 1, 2
 
 # Memisahkan dataset menjadi data latih dan data validasi dengan rasio 80:20
-X_train, X_test, y_train, y_test = train_test_split(X, y,
-test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
 
-model = Sequential([
-Input(shape=X_train.shape[1:]),
-Dense(1000, activation='relu'),
-Dense(500, activation='relu'),
-Dense(300, activation='relu'),
-Dense(3, activation='softmax')
-])
+model = Sequential(
+    [
+        Input(shape=X_train.shape[1:]),
+        Dense(1000, activation="relu"),
+        Dense(500, activation="relu"),
+        Dense(300, activation="relu"),
+        Dense(3, activation="softmax"),
+    ]
+)
+
 model.summary()
 
 model.compile(
-optimizer='adam',
-loss='sparse_categorical_crossentropy',
-metrics=['accuracy']
+    optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"]
 )
 
 history = model.fit(
-X_train, y_train,
-epochs=50,
-batch_size=32,
-validation_data=(X_test, y_test)
+    X_train, y_train, epochs=50, batch_size=32, validation_data=(X_test, y_test)
 )
 
 loss, accuracy = model.evaluate(X_test, y_test)
 print(f"Loss: {loss}, Accuracy: {accuracy}")
-pd.DataFrame(history.history).plot(figsize=(10,6))
+pd.DataFrame(history.history).plot(figsize=(10, 6))
 
 predictions = model.predict(X_test)
+
 # Mengambil indeks dari nilai probabilitas tertinggi untuk setiap prediksi
 predicted_classes = predictions.argmax(axis=1)
 print("Prediksi:", predicted_classes)
@@ -60,14 +61,22 @@ print("Label Asli:", y_test)
 
 # Buat confusion matrix
 cm = confusion_matrix(y_test, predicted_classes)
+
 # Visualisasikan confusion matrix
 plt.figure(figsize=(8, 6))
-sns.heatmap(cm, annot=True, fmt='d', cmap='Blues',
-xticklabels=label_encoder.classes_, yticklabels=label_encoder.classes_)
-plt.xlabel('Predicted')
-plt.ylabel('True')
-plt.title('Confusion Matrix')
+sns.heatmap(
+    cm,
+    annot=True,
+    fmt="d",
+    cmap="Blues",
+    xticklabels=label_encoder.classes_,
+    yticklabels=label_encoder.classes_,
+)
+plt.xlabel("Predicted")
+plt.ylabel("True")
+plt.title("Confusion Matrix")
 plt.show()
+
 
 # Fungsi untuk memprediksi data input baru
 def predict_new_data():
@@ -75,13 +84,13 @@ def predict_new_data():
     sepal_width = float(input("Masukkan sepal width: "))
     petal_length = float(input("Masukkan petal length: "))
     petal_width = float(input("Masukkan petal width: "))
-# Membuat data array baru
-    new_data = np.array([[sepal_length, sepal_width, petal_length,
-petal_width]])
-# Melakukan prediksi
+
+    # Membuat data array baru
+    new_data = np.array([[sepal_length, sepal_width, petal_length, petal_width]])
+    # Melakukan prediksi
     prediction = model.predict(new_data)
     predicted_class = prediction.argmax(axis=1)
-# Mengonversi hasil prediksi numerik menjadi label asli
+    # Mengonversi hasil prediksi numerik menjadi label asli
     predicted_label = label_encoder.inverse_transform(predicted_class)
     print(f"Prediksi kelas: {predicted_label[0]}")
 
